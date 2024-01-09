a4c577814fec86e3221de6ed5c417725b40c2cc5
chriswoodcn <chriswoodcn@aliyun.com>
2024-01-09 23:19:34 +0800

    [update] 迷宫 广度优先算法1

---
+++ examples/algorithm/labyrinth_test.go
@@ -0,0 +1,15 @@
+package algorithm
+
+import "testing"
+
+// 迷宫算法
+func TestLabyrinth(t *testing.T) {
+	// 算法逻辑
+	// 从第0步开始往外探索 1 2 ...
+	//           2
+	//       2  1   2
+	//  2  1   1   1  2
+	//      2   1  2
+	//           2
+
+}
