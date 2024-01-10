e5fca62f6700902ca5d9fe115a55cefcd3c11c00
chriswoodcn <chriswoodcn@aliyun.com>
2024-01-10 22:20:04 +0800

    [fix] customRequest  User-Agent  fixed

--- http/httpClient_test.go
+++ http/httpClient_test.go
@@ -2,7 +2,6 @@ package http
 
 import (
 	"fmt"
-	"io"
 	"net/http"
 	"net/http/httputil"
 	"testing"
@@ -25,17 +24,11 @@ func customRequest() {
 		nil,
 	)
 	request.Header.Add("User-Agent",
-		"Mozilla/5.0 (iPhone; CPU iPhone OS 16_6 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/16.6 Mobile/15E148 Safari/604.1\nThird-party cookie phaseout\nThe Issues panel now warns you about the cookies that will be affected by the upcoming deprecation and phaseout of third-party cookies.\nEffective Content Security Policy\nView the Content Security Policy details of an inspected frame in a new section in Application > Frames.\nImproved animation debugging\nClick to set playhead and resize timeline in the Animations panel.")
+		"Mozilla/5.0 (iPhone; CPU iPhone OS 16_6 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/16.6 Mobile/15E148 Safari/604.1")
 	resp, err := http.DefaultClient.Do(request)
 	if err != nil {
-		fmt.Println("http get error")
+		panic(err)
 	}
-	defer func(Body io.ReadCloser) {
-		err := Body.Close()
-		if err != nil {
-			fmt.Println("body close error")
-		}
-	}(resp.Body)
 	response, err := httputil.DumpResponse(resp, true)
 	fmt.Printf("%s ", response)
 }
